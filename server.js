92687cbe99cf808d586cb9ae63732a7d84ea86e3
HeikoKuschel <file:.git/config        core.logallrefupd>
2019-10-11 00:20:26 +0200

    rest

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 6000;
+const port = 3000;
 const public = '/public';
 const img = '/rec/img/';
 app.use(express.static('public'));
